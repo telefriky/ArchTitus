38475b76917e602de6f70b44efc82602cbd9183a
telefriky <49905355+telefriky@users.noreply.github.com>
2022-01-23 19:49:27 +0100

    Update 0-preinstall.sh
    anulamos toda la parte de configuración de disco

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -17,7 +17,7 @@ echo -ne "
   ██║  ██║██║  ██║╚██████╗██║  ██║   ██║   ██║   ██║   ╚██████╔╝███████║
   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝   ╚═╝    ╚═════╝ ╚══════╝
 -------------------------------------------------------------------------
-                    Automated Arch Linux Installer
+               Automated Arch Linux Installer ( version MYA )
 -------------------------------------------------------------------------
 
 Setting up mirrors for optimal download
@@ -35,30 +35,30 @@ echo -ne "
                     Setting up $iso mirrors for faster downloads
 -------------------------------------------------------------------------
 "
-reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
-mkdir /mnt &>/dev/null # Hiding error message if any
+#reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+#mkdir /mnt &>/dev/null # Hiding error message if any
 echo -ne "
 -------------------------------------------------------------------------
                     Installing Prerequisites
 -------------------------------------------------------------------------
 "
-pacman -S --noconfirm gptfdisk btrfs-progs
+#pacman -S --noconfirm gptfdisk btrfs-progs
 echo -ne "
 -------------------------------------------------------------------------
                     Formating Disk
 -------------------------------------------------------------------------
 "
 # disk prep
-sgdisk -Z ${DISK} # zap all on disk
-sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+#sgdisk -Z ${DISK} # zap all on disk
+#sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
 # create partitions
-sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
-sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
-if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
-    sgdisk -A 1:set:2 ${DISK}
-fi
+#sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
+#sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
+#sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+#if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
+#    sgdisk -A 1:set:2 ${DISK}
+#fi
 # make filesystems
 echo -ne "
 -------------------------------------------------------------------------
@@ -81,79 +81,79 @@ mountallsubvol () {
 }
 
 if [[ "${DISK}" =~ "nvme" ]]; then
-    partition2=${DISK}p2
-    partition3=${DISK}p3
+#    partition2=${DISK}p2
+#    partition3=${DISK}p3
 else
-    partition2=${DISK}2
-    partition3=${DISK}3
+#    partition2=${DISK}2
+#    partition3=${DISK}3
 fi
 
 if [[ "${FS}" == "btrfs" ]]; then
-    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
-    mkfs.btrfs -L ROOT ${partition3} -f
-    mount -t btrfs ${partition3} /mnt
+#    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+#    mkfs.btrfs -L ROOT ${partition3} -f
+#    mount -t btrfs ${partition3} /mnt
 elif [[ "${FS}" == "ext4" ]]; then
-    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
-    mkfs.ext4 -L ROOT ${partition3}
-    mount -t ext4 ${partition3} /mnt
+#    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+#    mkfs.ext4 -L ROOT ${partition3}
+#    mount -t ext4 ${partition3} /mnt
 elif [[ "${FS}" == "luks" ]]; then
-    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+#    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
 # enter luks password to cryptsetup and format root partition
-    echo -n "${luks_password}" | cryptsetup -y -v luksFormat ${partition3} -
+#    echo -n "${luks_password}" | cryptsetup -y -v luksFormat ${partition3} -
 # open luks container and ROOT will be place holder 
-    echo -n "${luks_password}" | cryptsetup open ${partition3} ROOT -
+#    echo -n "${luks_password}" | cryptsetup open ${partition3} ROOT -
 # now format that container
-    mkfs.btrfs -L ROOT /dev/mapper/ROOT
+#    mkfs.btrfs -L ROOT /dev/mapper/ROOT
 # create subvolumes for btrfs
-    mount -t btrfs /dev/mapper/ROOT /mnt
-    createsubvolumes       
-    umount /mnt
+#    mount -t btrfs /dev/mapper/ROOT /mnt
+#    createsubvolumes       
+#    umount /mnt
 # mount @ subvolume
-    mount -o ${mountoptions},subvol=@ /dev/mapper/ROOT /mnt
+#    mount -o ${mountoptions},subvol=@ /dev/mapper/ROOT /mnt
 # make directories home, .snapshots, var, tmp
-    mkdir -p /mnt/{home,var,tmp,.snapshots}
+#    mkdir -p /mnt/{home,var,tmp,.snapshots}
 # mount subvolumes
-    mountallsubvol
+#    mountallsubvol
 # store uuid of encrypted partition for grub
-    echo encryped_partition_uuid=$(blkid -s UUID -o value ${partition3}) >> setup.conf
+#    echo encryped_partition_uuid=$(blkid -s UUID -o value ${partition3}) >> setup.conf
 fi
 
 # checking if user selected btrfs
 if [[ ${FS} =~ "btrfs" ]]; then
-ls /mnt | xargs btrfs subvolume delete
-btrfs subvolume create /mnt/@
-umount /mnt
-mount -t btrfs -o subvol=@ -L ROOT /mnt
+#ls /mnt | xargs btrfs subvolume delete
+#btrfs subvolume create /mnt/@
+#umount /mnt
+#mount -t btrfs -o subvol=@ -L ROOT /mnt
 fi
 
 # mount target
-mkdir /mnt/boot
-mkdir /mnt/boot/efi
-mount -t vfat -L EFIBOOT /mnt/boot/
+#mkdir /mnt/boot
+#mkdir /mnt/boot/efi
+#mount -t vfat -L EFIBOOT /mnt/boot/
 
 if ! grep -qs '/mnt' /proc/mounts; then
-    echo "Drive is not mounted can not continue"
-    echo "Rebooting in 3 Seconds ..." && sleep 1
-    echo "Rebooting in 2 Seconds ..." && sleep 1
-    echo "Rebooting in 1 Second ..." && sleep 1
-    reboot now
+#    echo "Drive is not mounted can not continue"
+#    echo "Rebooting in 3 Seconds ..." && sleep 1
+#    echo "Rebooting in 2 Seconds ..." && sleep 1
+#    echo "Rebooting in 1 Second ..." && sleep 1
+#    reboot now
 fi
 echo -ne "
 -------------------------------------------------------------------------
                     Arch Install on Main Drive
 -------------------------------------------------------------------------
 "
-pacstrap /mnt base base-devel linux linux-firmware vim nano sudo archlinux-keyring wget libnewt --noconfirm --needed
-echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
-cp -R ${SCRIPT_DIR} /mnt/root/ArchTitus
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+#pacstrap /mnt base base-devel linux linux-firmware vim nano sudo archlinux-keyring wget libnewt --noconfirm --needed
+#echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
+#cp -R ${SCRIPT_DIR} /mnt/root/ArchTitus
+#cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 echo -ne "
 -------------------------------------------------------------------------
                     GRUB BIOS Bootloader Install & Check
 -------------------------------------------------------------------------
 "
 if [[ ! -d "/sys/firmware/efi" ]]; then
-    grub-install --boot-directory=/mnt/boot ${DISK}
+#    grub-install --boot-directory=/mnt/boot ${DISK}
 fi
 echo -ne "
 -------------------------------------------------------------------------
@@ -163,14 +163,14 @@ echo -ne "
 TOTALMEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
 if [[  $TOTALMEM -lt 8000000 ]]; then
     # Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
-    mkdir /mnt/opt/swap # make a dir that we can apply NOCOW to to make it btrfs-friendly.
-    chattr +C /mnt/opt/swap # apply NOCOW, btrfs needs that.
-    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=2048 status=progress
-    chmod 600 /mnt/opt/swap/swapfile # set permissions.
-    chown root /mnt/opt/swap/swapfile
-    mkswap /mnt/opt/swap/swapfile
-    swapon /mnt/opt/swap/swapfile
-    # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
+#    mkdir /mnt/opt/swap # make a dir that we can apply NOCOW to to make it btrfs-friendly.
+#    chattr +C /mnt/opt/swap # apply NOCOW, btrfs needs that.
+#    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=2048 status=progress
+#    chmod 600 /mnt/opt/swap/swapfile # set permissions.
+#    chown root /mnt/opt/swap/swapfile
+#    mkswap /mnt/opt/swap/swapfile
+#    swapon /mnt/opt/swap/swapfile
+#    # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
     echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab # Add swap to fstab, so it KEEPS working after installation.
 fi
 echo -ne "
